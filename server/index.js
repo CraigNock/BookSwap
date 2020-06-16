db76382284485bc274ad60c31635b38f3c4389fb
Craig Nockels <cnockels@gmail.com>
2020-06-16 12:22:15 -0400

    concurrent

--- server/index.js
+++ server/index.js
@@ -1,34 +1,15 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-var bodyParser = __importStar(require("body-parser"));
+var body_parser_1 = __importDefault(require("body-parser"));
 var cors_1 = __importDefault(require("cors"));
 var app = express_1.default();
 var PORT = process.env.PORT || 8000;
 //or without body-parser (may be built into express depending on version):> app.use(express.json()) does same thing;
-app.use(bodyParser.json());
+app.use(body_parser_1.default.json());
 app.use(cors_1.default());
 // may not need following if using cors
 app.use(function (req, res, next) {
