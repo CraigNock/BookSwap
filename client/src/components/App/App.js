7b5d6952b3fffd7a76cc499bfbd10c1e1aab2b5d
Craig Nockels <cnockels@gmail.com>
2020-06-25 21:59:50 -0400

    index test

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -9,7 +9,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importDefault(require("react"));
 var styled_components_1 = __importDefault(require("styled-components"));
-var GlobalStyles_1 = __importDefault(require("../GlobalStyles"));
+var GlobalStyles_1 = __importDefault(require("../../GlobalStyles"));
 var App = function () {
     return (react_1.default.createElement(Wrapper, null,
         react_1.default.createElement(GlobalStyles_1.default, null),
--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Global from '../GlobalStyles';
+import Global from '../../GlobalStyles';
 
 const App = () => {
   return (
